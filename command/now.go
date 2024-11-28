9fc71b94076c4f99ab1497a870be4ca6ec22090a
rsora <sora.roberto@gmail.com>
2024-11-28 18:38:29 +0100

    Rename busines package to core to allow for a visual layered structure

--- command/now.go
+++ command/now.go
@@ -4,8 +4,8 @@ import (
 	"flag"
 	"fmt"
 
-	"github.com/robizz/his-tor-y/business"
 	"github.com/robizz/his-tor-y/conf"
+	"github.com/robizz/his-tor-y/core"
 )
 
 // Command struct
@@ -36,7 +36,7 @@ func (n *Now) Parse(conf conf.Config, args []string) error {
 // implements command interface in main package
 func (n *Now) Execute() int {
 
-	out, err := business.Now(n.Conf.ExitNode.DownloadURLTemplate, n.StartDate, n.EndDate)
+	out, err := core.Now(n.Conf.ExitNode.DownloadURLTemplate, n.StartDate, n.EndDate)
 
 	if err != nil {
 		fmt.Printf("Error %v\n", err)
--- core/now_test.go
+++ core/now_test.go
@@ -1,4 +1,4 @@
-package business
+package core
 
 import (
 	"bufio"
